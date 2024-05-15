3649e3da610e2f627524a08435c67f36bb922419
Tyron-P <tyron.pyle@gmail.com>
2024-05-15 10:10:05 +0100

    information displayed

--- my-project/src/Components/FetchTitleDetails.jsx
+++ my-project/src/Components/FetchTitleDetails.jsx
@@ -21,7 +21,7 @@ export const useFetchTitleDetails = (titleId, urlMock) => {
     }
 
     console.log(data);
-  }, []);
+  });
 
   return { data, isPending };
 };
--- my-project/src/pages/MovieDetails.jsx
+++ my-project/src/pages/MovieDetails.jsx
@@ -5,12 +5,36 @@ import "./website.css";
 function MovieDetails({ movieId }) {
   const urlMock = "http://localhost:8000/titleDetails";
   const { data, isPending } = useFetchTitleDetails(movieId, urlMock);
+
   return (
     <div className="Home">
       <header>
         <Navbar />
       </header>
-      <div className="Home-body">test</div>
+      <div className="Home-body">
+        {!isPending && (
+          <div className="container">
+            <div className="row row-cols-2">
+              <div className="col">
+                <img src={data.poster} className="img-fluid" alt="..." />
+              </div>
+              <div className="col">
+                <p className="text-sm-start">{data.plot_overview}</p>
+              </div>
+              <div className="col">
+                {data.title} <br />
+                <div>{`User Rating: ${data.user_rating}`}</div>
+              </div>
+              <div className="col">
+                Watch Trailer:
+                <a href={data.trailer} className="link">
+                  {data.trailer}
+                </a>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
